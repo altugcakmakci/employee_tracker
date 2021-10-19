02bfca1e2a5319fc7f597291c11b8a2e9d5eafbd
Altug Cakmakci <altugcakmakci@gmail.com>
2021-10-19 13:37:29 -0400

    readme file added

---
+++ README.md
@@ -0,0 +1,50 @@
+# employee_tracker
+
+## Description
+
+This application manages the departments, roles, and employees of a company.
+The application uses inquirer and asks a list of questions to the user 
+allowing the user to display the data, add new rows, and update data
+in the database. 
+
+## Table of Contents
+
+- [Installation](#installation)
+
+- [Usage](#usage)
+
+- [License](#license)
+
+- [Contributing](#contributing)
+
+- [Tests](#tests)
+
+- [Questions](#questions)
+
+- [Walkthrough](#walkthrough)
+
+## Installation
+
+To install neessary dependencies, run the following command
+npm i 
+
+## Usage
+The user can use the repo without any specific knowledge.
+
+## License
+No license.
+
+## Contributing
+No contribution is expected.
+
+## Tests
+
+No test script delivered
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at alt@yahoo.com. 
+You can find more of my work at (https://github.com/altugcakmakci)
+
+## Walkthrough
+
--- model/employee.js
+++ model/employee.js
@@ -1,55 +1,54 @@
-const {connection} = require('./db');
+const { connection } = require('./db');
 
-function readEmployeeAll (callback) {
-    connection.query('SELECT * FROM EMPLOYEE', (err,rows) => {
-        if(err) throw err;
-      
+function readEmployeeAll(callback) {
+    connection.query('SELECT * FROM EMPLOYEE', (err, rows) => {
+        if (err) throw err;
+
         return callback(rows)
-      });
+    });
+}
+
+function insertEmployee(employee, callback) {
+    console.log(employee);
+    if (employee.managerId === -1) {
+        connection.query('INSERT INTO EMPLOYEE (first_name,last_name,role_id) VALUES (?,?,?)', [employee.firstName, employee.lastName, employee.roleId], (err, res) => {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            callback("Employee added.");
+        });
+    } else {
+        connection.query('INSERT INTO EMPLOYEE (first_name,last_name,role_id,manager_id) VALUES (?,?,?,?)', [employee.firstName, employee.lastName, employee.roleId, employee.managerId], (err, res) => {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            callback("Employee added.");
+        });
     }
 
-    function insertEmployee(employee, callback) {
-        console.log(employee);
-        if (employee.managerId===-1){
-            connection.query('INSERT INTO EMPLOYEE (first_name,last_name,role_id) VALUES (?,?,?)', [employee.firstName, employee.lastName,employee.roleId], (err, res) => {
-                if (err) {
-                    console.log(err);
-                    throw err;
-                }
-                callback("Employee added.");
-            });
-        } else {
-            connection.query('INSERT INTO EMPLOYEE (first_name,last_name,role_id,manager_id) VALUES (?,?,?,?)', [employee.firstName, employee.lastName,employee.roleId,employee.managerId], (err, res) => {
-                if (err) {
-                    console.log(err);
-                    throw err;
-                }
-                callback("Employee added.");
-            }); 
+}
+
+function deleteEmployee(id, callback) {
+    connection.query('DELETE FROM EMPLOYEE WHERE ID = ?', [id], (err, res) => {
+        if (err) {
+            console.log(err);
+            throw err;
         }
+        callback("Employee deleted.");
+    });
+}
 
-    }
-    
-    function deleteEmployee(id, callback) {
-        connection.query('DELETE FROM EMPLOYEE WHERE ID = ?', [id], (err, res) => {
-            if (err) {
-                console.log(err);
-                throw err;
-            }
-            callback("Employee deleted.");
-        });
-    }
-    
-    function updateEmployeeRole(id, roleId, callback) {
-        connection.query('UPDATE EMPLOYEE SET ROLE_ID = ? WHERE ID = ?', [roleId, id ], (err, res) => {
-            if (err) {
-                console.log(err);
-                throw err;
-            }
-            callback("Employee updated.");
-        });
-    }
-    
-    module.exports = { readEmployeeAll, insertEmployee, deleteEmployee, updateEmployeeRole };
-    
-    
+function updateEmployeeRole(id, roleId, callback) {
+    connection.query('UPDATE EMPLOYEE SET ROLE_ID = ? WHERE ID = ?', [roleId, id], (err, res) => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        callback("Employee updated.");
+    });
+}
+
+module.exports = { readEmployeeAll, insertEmployee, deleteEmployee, updateEmployeeRole };
+
